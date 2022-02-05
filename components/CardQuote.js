50c77d35f696e8ec6bc42735b89a6382ff584a21
Jamilur Rahman <r.jamilur96@gmail.com>
2022-02-06 03:31:41 +0600

    card ready, sentiment works

--- components/CardQuote.js
+++ components/CardQuote.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Card, Icon } from 'react-native-elements';
 
-function CardQuote() {
-  return <div></div>;
+function CardQuote({quote, author}) {
+  return (
+    <View style={styles.container}>
+       <Card containerStyle={{ marginTop: 15 }}>
+          <Text style={styles.fonts} h1>
+            {quote}
+          </Text>
+          <Text style={styles.fonts}>{author}</Text>
+          <Card.Divider />
+        </Card>
+    </View>
+  );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  fonts: {
+    marginBottom: 8,
+  }
+});
 
 export default CardQuote;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,51 @@
-import React from 'react';
-import { SafeAreaView, Text, View, TouchableOpacity } from 'react-native';
-import tw from 'tailwind-react-native-classnames'
-import AddQuote from '../components/AddQuote';
-import Header from '../components/Header';
+import React from "react";
+import { SafeAreaView, View, TouchableOpacity, StyleSheet } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import AddQuote from "../components/AddQuote";
+import Header from "../components/Header";
+import Sentiment from "sentiment";
+
+import { Text, Card, Icon } from 'react-native-elements';
 
 function HomeScreen() {
   const url = "https://type.fit/api/quotes";
-  
-  return(
-      <SafeAreaView style={tw`bg-white h-full`}>
-        <View style={tw`p-16 bg-red-100`}>
-        <Header title='Quotes of the Day'/>
-        </View>
 
-        <AddQuote />
-      </SafeAreaView>
+  const sentiment = new Sentiment();
+  const string = sentiment.analyze("What a lovely day!");
+  console.log(string.score);
+
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-16 bg-red-100`}>
+        <Header title="Quotes of the Day" />
+      </View>
+      <Text>Your Sentiment Score is : {string.score}</Text>
+      
+
+      <View style={styles.container}>
+        <Card containerStyle={{ marginTop: 15, padding: 20 }}>
+          <Text style={styles.fonts} h1>
+            A very random quote
+          </Text>
+          <Text style={styles.fonts}>random author</Text>
+          <Card.Divider />
+        </Card>
+      </View>
+
+      <AddQuote />
+    </SafeAreaView>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  fonts: {
+    marginBottom: 15,
+    textAlign: 'center',
+  }
+});
 
 export default HomeScreen;
