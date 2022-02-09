0e6084d00f0209894e6a2d3e8b5897f37417cb79
Jamilur Rahman <r.jamilur96@gmail.com>
2022-02-10 03:45:31 +0600

    need to render list of insights

--- screens/MyInsightsScreen.js
+++ screens/MyInsightsScreen.js
@@ -1,16 +1,70 @@
-import React from 'react';
-import { SafeAreaView, View } from 'react-native';
-import tw from 'tailwind-react-native-classnames'
-import Header from '../components/Header';
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, View, StyleSheet, FlatList } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import Header from "../components/Header";
+import SavedCardQuote from "../components/SavedCardQuote";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 function MyInsightsScreen() {
-  return(
-      <SafeAreaView>
-         <View style={tw`p-16 bg-gray-100`}>
+  const [savedquotes, setSavedQuotes] = useState([]);
+
+  const renderQuote = ({ item: { savedQuote, savedAuthor } }) => {
+    return (
+      <SavedCardQuote
+        style={styles.container}
+        quote={savedQuote}
+        author={savedAuthor}
+        sentimentValue={savedQuote}
+      />
+    );
+  };
+
+  const getSavedQuotes = async () => {
+    try {
+      const quotes = await AsyncStorage.getItem("myquote");
+      setSavedQuotes(JSON.parse(quotes));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const clearAll = async () => {
+    try {
+      await AsyncStorage.clear();
+    } catch (e) {
+      // clear error
+    }
+    console.log("Done.");
+  };
+
+  useEffect(()=>{
+    getSavedQuotes()
+    //clearAll();
+  }, [savedquotes])
+
+
+  return (
+    <SafeAreaView style={tw`bg-gray-100 h-full`}>
+      <View style={tw`p-16 bg-gray-100`}>
         <Header title="My Insights" />
       </View>
-      </SafeAreaView>
+
+      <FlatList
+        data={savedquotes}
+        renderItem={renderQuote}
+        keyExtractor={(quote, index) => index.toString()}
+        contentContainerStyle={{ paddingBottom: 20 }}
+      />
+    </SafeAreaView>
   );
 }
 
 export default MyInsightsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+  },
+});
